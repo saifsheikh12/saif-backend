d8fd4f8940a0fe0e8fb336880adc8b507a37eb42
Saif <saifuddin6205567@gmail.com>
2022-08-10 14:06:02 +0530

    missing number

--- src/index.js
+++ src/index.js
@@ -8,8 +8,55 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', route);
+app.use('/', route)
+
+//sol1 find missing number 4
+
+app.get("sol1",function (req ,res){
+    let arr =[1,2,3,5,6,7]
+    
+    let total =0;
+    for(var i in arr){
+        total +=arr[i];
+        
+    }
+    let lastdigit=arr.pop()
+    let consecutivesum = lastdigit * (lastdigit+1)/2
+    let missingNumber = consecutivesum-total
+
+    res.send ({data:missingNumber} );
+})
+
+
+
+//sol2 36 missing number
+
+
+app.get("sol2",function (req ,res){
+    let arr =[33,34,35,37,38]
+    let len=arr.length
+    
+    let total =0;
+    for(var i in arr){
+        total =total + arr[i];
+        
+    }
+    let firstDigit=arr[0]
+    let lastdigit=arr.pop()
+    let consecutivesum = (len + 1)*(firstDigit+lastdigit) /2
+    let missingNumber = consecutivesum-total
+
+    res.send ({data:missingNumber} );
+})
+
+
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+
+
+
+
