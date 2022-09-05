11a4c5fff94532178f6671a03c0744bd8313ceb2
vipulradon <you@example.com>
2022-09-05 17:09:33 +0530

    addedcreateauthor

---
+++ src/models/blogModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const objectId= mongoose.Schema.Types.ObjectId;
+
+
+
+const blogSchema= new mongoose.Schema({
+title:{
+    type:String,
+    required:true
+},
+body:{
+    type:String,
+    required:true
+},
+authorId:{
+type:objectId,
+ref:"authors"
+},
+tags:{
+    type:[String]
+},
+category:{
+    type:String,
+    required:true
+},
+subcategory:[String],
+deletedAt:Date,
+isDeleted:{
+    type:Boolean,
+    default:false
+},
+publishedAt:Date,
+isPublished:{
+    type:Boolean,
+    default:false
+}
+
+
+},{timestamps:true});
+
+
+module.exports= mongoose.model("blog",blogSchema);
---
+++ src/routes/route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const authorController = require("../controllers/authorController");
+
+router.post("/authors",authorController.createAuthor);
+
+
+
+module.exports = router;
