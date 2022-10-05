77b510bca859a88070588ee92f6ecb06967affe1
ajijsk <ajijsk240@gmail.com>
2022-10-06 01:53:30 +0530

    redis remove from urlCon.

--- src/controllers/urlControllers.js
+++ src/controllers/urlControllers.js
@@ -1,30 +1,10 @@
 const urlModel = require("../models/urlModel");
 const shortid = require('shortid')
 const validUrl = require('valid-url')
-const {redisClient,SET_ASYNC,GET_ASYNC}=require("./redis")
+const {SET_ASYNC,GET_ASYNC}=require("./redis")
 const isUrl = require("is-valid-http-url")
 
-// const { promisify } = require("util");
 
-
-// //Connect to redis
-// const redisClient = redis.createClient(
-//   13190,
-//   "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
-//   { no_ready_check: true }
-// );
-// redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
-//   if (err) throw err;
-// });
-
-// redisClient.on("connect", async function () {
-//   console.log("Connected to Redis..");
-// });
- 
-// //Connection setup for redis
-
-// const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
-// const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 // Create Url
 
